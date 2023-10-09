168a5c4d42464413008b33156c174fb48b42eea1
王二 <18530117331@163.com>
2023-10-10 00:13:42 +0800

    增加图片

---
+++ Assets/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using IngameDebugConsole;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        DebugLogConsole.AddCommand("测试菜单/测试","报错", () => throw new Exception("测试异常"));
+    }
+}
